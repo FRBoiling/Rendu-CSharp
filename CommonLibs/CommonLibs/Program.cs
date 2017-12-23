9a299ce30afd3fc8d7f0261740eaa1c3e5bc71d4
Boiling <free22858@live.com>
2017-12-23 13:51:25 +0800

    xml test code

--- CommonLibs/CommonLibs/Program.cs
+++ CommonLibs/CommonLibs/Program.cs
@@ -1,25 +1,12 @@
-﻿using DataParserLib;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace CommonLibs
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
-            string filename = "D:\\GitHub\\ServerCluster-master\\Bin\\Data\\Xml\\ServerConfig.xml";
-            XmlDataManager.Inst.Parse(filename);
-
-            DataList data = XmlDataManager.Inst.GetDataList("ServerConfig");
-            foreach (var item in data)
-            {
-                Console.WriteLine("key {0} value {1}", item.Key, item.Value.Name);
-            }
-
+            TestXml();
             Console.ReadKey();
         }
     }
---
+++ CommonLibs/CommonLibs/Program_xml.cs
@@ -0,0 +1,20 @@
+﻿using DataParserLib;
+using System;
+
+namespace CommonLibs
+{
+    partial class  Program
+    {
+        public static void TestXml()
+        {
+            string filename = "D:\\GitHub\\ServerCluster-master\\Bin\\Data\\Xml\\ServerConfig.xml";
+            XmlDataManager.Inst.Parse(filename);
+
+            DataList data = XmlDataManager.Inst.GetDataList("ServerConfig");
+            foreach (var item in data)
+            {
+                Console.WriteLine("key {0} value {1}", item.Key, item.Value.Name);
+            }
+        }
+    }
+}
