46aad5546bfcc616c49b8ac0e3da9f8f2cc1dd33
fanrong <FRee22858@live.com>
2018-04-19 01:04:00 +0800

    update

--- Protocol/ClientProtocol/Client/Protocol/CG.code.cs
+++ Protocol/ClientProtocol/Client/Protocol/CG.code.cs
@@ -1,18 +1,17 @@
-namespace Protocol.Client.C2G
-{
-    public partial class MSG_C2G_Heartbeat { }
-    public partial class MSG_C2G_GetEncryptKey { }
-    public partial class MSG_C2G_Login { }
-    public partial class MSG_C2G_CreateRole { }
-    public class Api
-    {
-        static public void GenerateId()
-        {
-            Engine.Foundation.Id<Protocol.Client.C2G.MSG_C2G_Heartbeat>.Value = 0xF200001;
-            Engine.Foundation.Id<Protocol.Client.C2G.MSG_C2G_GetEncryptKey>.Value = 0xF200010;
-            Engine.Foundation.Id<Protocol.Client.C2G.MSG_C2G_Login>.Value = 0xF200101;
-            Engine.Foundation.Id<Protocol.Client.C2G.MSG_C2G_CreateRole>.Value = 0xF200102;
-        }
-    }
+using System;
+namespace Protocol.Client.C2G {
+
+	public partial class MSG_C2G_Heartbeat {}
+	public partial class MSG_C2G_GetEncryptKey {}
+	public partial class MSG_C2G_Login {}
+	public partial class MSG_C2G_CreateRole {}
+	public class Api {
+		static public void GenerateId() {
+			Engine.Foundation.Id<Protocol.Client.C2G.MSG_C2G_Heartbeat>.Value = 0xF200001;
+			Engine.Foundation.Id<Protocol.Client.C2G.MSG_C2G_GetEncryptKey>.Value = 0xF200010;
+			Engine.Foundation.Id<Protocol.Client.C2G.MSG_C2G_Login>.Value = 0xF200101;
+			Engine.Foundation.Id<Protocol.Client.C2G.MSG_C2G_CreateRole>.Value = 0xF200102;
+		}
+	}
 
 }
--- Protocol/ProtoGenerater/Program.cs
+++ Protocol/ProtoGenerater/Program.cs
@@ -15,50 +15,110 @@ namespace ProtoGenerater
             CSharpMsgIdModel idModel = new CSharpMsgIdModel();
             StringBuilder strGenerateCode = idModel.GenerateCode_ID();
             string codeFileName = "Id.cs";
+            string fileName = "";
             string codeFileFullName = @"..\..\Bin\" + codeFileName;
             FileInfo fCodeFileName = new FileInfo(codeFileFullName);
             StreamWriter wResponseRecv = fCodeFileName.CreateText();
             wResponseRecv.WriteLine(strGenerateCode.ToString());
             wResponseRecv.Close();
 
-            string[] files = Directory.GetFiles(@"..\..\Bin\", "*.code", SearchOption.AllDirectories);
-
+            string[] files = Directory.GetFiles(@"..\..\..\BuildClientProtocol\", "*.code", SearchOption.AllDirectories);
+            StringBuilder protoCode = new StringBuilder();
             string packageName = "Protocol.Client.C2G";
             Dictionary<string, string> dicMsg = new Dictionary<string, string>();
-            foreach (var item in files)
+
+            foreach (var file in files)
             {
-                string[] lines = File.ReadAllLines(item);
+                int index = file.LastIndexOf("\\");
+                codeFileName = file.Substring(index + 1);
+
+                IEnumerable<string> lines = File.ReadAllLines(file);
+
+                protoCode.Clear();
+                dicMsg.Clear();
+
                 foreach (string line in lines)
                 {
-                    int index = line.IndexOf(" ");
-                    if (index >0)
+                    string strProtoline = "";
+
+                    if (line.StartsWith("package"))
                     {
-                        string key = line.Substring(0, index);
-                        if ("package" == key)
+                        strProtoline = line;
+                        protoCode.Append(strProtoline);
+                        protoCode.Append(Environment.NewLine);
+
+                        index = line.IndexOf(" ");
+                        packageName = line.Substring(index);
+                        packageName = packageName.Replace(" ", "");
+                        packageName = packageName.Replace(";", "");
+                    }
+                    else if (line.StartsWith("message"))
+                    {
+                        strProtoline = line;
+                        var arr = strProtoline.Split(' ');
+                        List<string> strFormat = new List<string>();
+                        if (arr.Length > 2)
                         {
-                            packageName = line.Substring(index);
-                            packageName = packageName.Replace(";", "");
-                        }
-                        else if ("message" == key)
-                        {
-                            string messageNameAndId = line.Substring(index+1);
-                            var arr = messageNameAndId.Split(' ');
-                            if (arr.Length > 1)
+                            foreach (var v in arr)
                             {
-                                dicMsg.Add(arr[0], arr[1]);
+                                if (v == " ")
+                                {
+                                }
+                                else
+                                {
+                                    strFormat.Add(v);
+                                }
+                            }
+                            if (strFormat.Count == 3)
+                            {
+                                if (arr[0] == "message")
+                                {
+                                    dicMsg.Add(arr[1], arr[2]);
+                                    strProtoline = strProtoline.Replace(arr[2], "");
+                                    protoCode.Append(Environment.NewLine);
+                                    protoCode.Append(strProtoline);
+                                    protoCode.Append(Environment.NewLine);
+                                }
                             }
                         }
                     }
+                    else if (line.StartsWith("//"))
+                    {
+                    }
+                    else
+                    {
+                        strProtoline = line;
+                        if (string.IsNullOrEmpty(strProtoline)||string.IsNullOrWhiteSpace(strProtoline))
+                        {
+                        }
+                        else
+                        {
+                            protoCode.Append(strProtoline);
+                            protoCode.Append(Environment.NewLine);
+                        }
+                    }
                 }
+
+                strGenerateCode = idModel.GenerateCode_GenerateId(packageName, dicMsg);
+                fileName = codeFileName + ".cs";
+                codeFileFullName = @"..\..\Bin\\" + fileName;
+                fCodeFileName = new FileInfo(codeFileFullName);
+                wResponseRecv = fCodeFileName.CreateText();
+                wResponseRecv.WriteLine(strGenerateCode.ToString());
+                wResponseRecv.Close();
+
+                strGenerateCode = protoCode;
+                fileName = codeFileName + ".proto";
+                codeFileFullName = @"..\..\Bin\\" + fileName;
+                fCodeFileName = new FileInfo(codeFileFullName);
+                wResponseRecv = fCodeFileName.CreateText();
+                wResponseRecv.WriteLine(strGenerateCode.ToString());
+                wResponseRecv.Close();
             }
-     
-            strGenerateCode = idModel.GenerateCode_GenerateId(packageName,dicMsg);
-            codeFileName = "CG.code.cs";
-            codeFileFullName = @"..\..\Bin\" + codeFileName;
-            fCodeFileName = new FileInfo(codeFileFullName);
-            wResponseRecv = fCodeFileName.CreateText();
-            wResponseRecv.WriteLine(strGenerateCode.ToString());
-            wResponseRecv.Close();
+
+
+
+
         }
     }
 }
