eea6573b3e7b893db99180e91f4d576815bcecd3
Boil <FRee22858@live.com>
2019-10-26 19:02:51 +0800

    NF[框架] 框架结构调整

---
+++ Unity/Assets/Sources/GameController.cs
@@ -0,0 +1,32 @@
+﻿using Sources.Systems;
+using UnityEngine;
+
+namespace Sources
+{
+    public class GameController : MonoBehaviour
+    {
+        Entitas.Systems _systems;
+
+        void Start()
+        {
+            // get a reference to the contexts
+            var contexts = Contexts.sharedInstance;
+        
+            // create the systems by creating individual features
+            _systems = new Feature("Systems")
+                .Add(new HelloWorldFeature(contexts));
+
+            // call Initialize() on all of the IInitializeSystems
+            _systems.Initialize();
+        }
+
+        void Update()
+        {
+            // call Execute() on all the IExecuteSystems and 
+            // ReactiveSystems that were triggered last frame
+            _systems.Execute();
+            // call cleanup() on all the ICleanupSystems
+            _systems.Cleanup();
+        }
+    }
+}
---
+++ Unity/Assets/Sources/Systems/HelloWorldFeature.cs
@@ -0,0 +1,13 @@
+﻿namespace Sources.Systems
+{
+    public class HelloWorldFeature : Feature
+    {
+        public HelloWorldFeature(Contexts contexts) : base ("Tutorial Systems")
+        {
+            Add(new HelloWorldSystem(contexts));
+            Add(new LogMouseClickSystem(contexts)); // new system
+            Add(new DebugMessageSystem(contexts));
+            Add(new CleanupDebugMessageSystem(contexts)); // new system (we want this to run last)
+        }
+    }
+}
