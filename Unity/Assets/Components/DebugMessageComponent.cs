d9b7791d533003896139ab22a38ffe705141db70
Boil <FRee22858@live.com>
2019-10-28 16:58:07 +0800

    NF[框架] entitas 框架搭建

---
+++ Unity/Assets/Components/DebugMessageComponent.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Unity.Entities;
+
+[Serializable]
+public struct DebugMessageComponent : IComponentData
+{
+   public string message;
+}
+
---
+++ Unity/Assets/Scenes/Launcher.cs
@@ -0,0 +1,46 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class Launcher:MonoBehaviour
+{
+//    public static EntityArchetype archetype1 { get; private set; }
+//    public static EntityArchetype archetype2 { get; private set; }
+//
+//    // This attribute allows us to not use MonoBehaviours to instantiate entities
+//    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+//    public static void InitializeBeforeScene()
+//    {
+//        var em = World.Active.GetOrCreateManager<EntityManager>(); // EntityManager manages all entities in world
+//        CreateArchetypes(em); // we need to set archetype first
+//    }
+//
+//    // This attribute allows us to not use MonoBehaviours to instantiate entities
+//    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+//    public static void InitializeAfterScene()
+//    {
+//        var em = World.Active.GetOrCreateManager<EntityManager>(); // EntityManager manages all entities in world
+//    }
+//    
+//    private static void CreateArchetypes(EntityManager em)
+//    {
+////        var heading = ComponentType.Create<Heading>();
+////        var health = ComponentType.Create<Health>();
+////        var moveSpeed = ComponentType.Create<MoveSpeed>();
+////
+////        archetype1 = em.CreateArchetype(pos, heading, health, moveSpeed);
+////        archetype2 = em.CreateArchetype(pos, heading, moveSpeed);
+//        // that's exactly how you set your entities archetype, it's like LEGO
+//    }
+    public GameObject Prefab;
+    void Start()
+    {
+        // Create entity prefab from the game object hierarchy once
+        Entity prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(Prefab, World.Active);
+        var entityManager = World.Active.EntityManager;
+        
+        var instance = entityManager.Instantiate(prefab);
+        entityManager.SetComponentData(instance, new DebugMessageComponent());
+        entityManager.AddComponentData(instance, new MouseClickComponent());
+    }
+
+}
